43ba141b3c2b3d00f216bdda181d9424c074e120
shaheed <shaheed.alltechnician@gmail.com>
2025-07-23 21:17:39 +0530

    footer height reduced

--- pdf-chat/app/page.tsx
+++ pdf-chat/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
           <Chat />
         </div>
       </div>
-      <div className=" p-4 bg-black   flex h-14 items-center justify-between supports-backdrop-blur:bg-background/60 sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur">
+      <div className=" p-4 bg-black   flex h-1 items-center justify-between supports-backdrop-blur:bg-background/60 sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur">
         <span className="font-bold text-white "></span>
       </div>
     </main>
